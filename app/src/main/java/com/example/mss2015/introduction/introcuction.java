792453c4f4ac9995d527cfd1e66da44499513f1e
KAILASHRATHORE1997 <kailashrathore551997@gmail.com>
2018-03-21 09:53:08 +0530

    commited

--- app/src/main/java/com/example/mss2015/introduction/introcuction.java
+++ app/src/main/java/com/example/mss2015/introduction/introcuction.java
@@ -1,5 +1,5 @@
 package com.example.mss2015.introduction;
-import android.content.Context;
+   import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.net.Uri;
@@ -50,7 +50,7 @@ public class introcuction extends AppCompatActivity
             finish();
         }
         if(Build.VERSION.SDK_INT>=21)
-        {
+         {
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         }
         setContentView(R.layout.activity_introcuction);
@@ -110,7 +110,7 @@ public class introcuction extends AppCompatActivity
         for(int i=0; i<dots.length; i++)
         {
            dots[i] = new TextView(this);
-            dots[i].setText(Html.fromHtml("&/h226"));
+            dots[i].setText(Html.fromHtml("&#8226;"));
             dots[i].setTextSize(35);
             dots[i].setTextColor(colorInactive[position]);
             dotsLayout.addView(dots[i]);
